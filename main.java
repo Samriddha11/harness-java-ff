5ebcb0869ce94eb89836d7e40e57f3e9a8ddb994
Samriddha11 <155475136+Samriddha11@users.noreply.github.com>
2024-05-24 11:45:03 +0530

    Update main.java

--- main.java
+++ main.java
@@ -31,12 +31,12 @@ public class Main {
 
             // Create targets
             List<Target> targets = new ArrayList<>();
-            targets.add(Target.builder().identifier("Users").name("Users").attribute("country", "US").build());
-            targets.add(Target.builder().identifier("Users").name("Users").attribute("version", "0.0.1").build());
-            targets.add(Target.builder().identifier("Users").name("Users").attribute("device", "ios").build());
-            targets.add(Target.builder().identifier("Users").name("Users").attribute("country", "UK").build());
-            targets.add(Target.builder().identifier("Users").name("Users").attribute("version", "0.0.2").build());
-            targets.add(Target.builder().identifier("Users").name("Users").attribute("country", "UK").attribute("version", "0.0.1").build());
+            targets.add(Target.builder().identifier("usUsers").name("usUsers").attribute("country", "US").build());
+            targets.add(Target.builder().identifier("BetaUsers").name("BetaUsers").attribute("version", "0.0.1").build());
+            targets.add(Target.builder().identifier("iosUsers").name("iosUsers").attribute("device", "ios").build());
+            targets.add(Target.builder().identifier("ukUsers").name("ukUsers").attribute("country", "UK").build());
+            targets.add(Target.builder().identifier("gaUsers").name("gaUsers").attribute("version", "0.0.2").build());
+            targets.add(Target.builder().identifier("CustomUser").name("CustomUser").attribute("country", "UK").attribute("version", "0.0.1").build());
 
             // Schedule a single task to report flag status for all targets
             scheduler.scheduleAtFixedRate(
